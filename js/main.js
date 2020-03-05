9db249678d018870a035c00577fa7340330f0c4e
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2020-03-05 13:29:04 -0700

    displayScreen functionality

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
-$('#advanced_button').click(()=>
-    { 
-        $('#orig_warning').hide();
-        $('#advanced_screen').show();
+let screens = [
+    'orig_warning',
+    'advanced_screen'
+];
+
+function displayScreen(screen)
+{
+    for(let i = 0; i < screens.length; i++)
+    {
+        if(screen === screens[i])
+        {
+            $('#'+screens[i]).show();
+        }
+        else
+        {
+            $('#'+screens[i]).hide();
+        }
+    }
+}
+
+$(() => { displayScreen(screens[0]); });
+
+$('#advanced_button').click(() => 
+    {
+        displayScreen('advanced_screen');
     }
 );
